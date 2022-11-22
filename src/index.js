9b0bb8a661924d822a576a4d183f06da961bc91e
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-11-22 23:08:37 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { App } from 'components/App';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 
-const API = '75b5a9ee57ad6476a1c136f37b30d941'
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
      <BrowserRouter>
