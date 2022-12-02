8b89a80ef3108f76df6ce2928d6d21f02b2e9af3
movitalii <103452623+movitalii@users.noreply.github.com>
2022-12-02 20:44:41 +0000

    Deploying to gh-pages from @ movitalii/goit-react-hw-05-movies@2abd12f008f09c19453f8fdc4f3834d303ea1618 🚀

---
+++ static/js/666.50e425e6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[666],{666:function(o,t,n){n.r(t),n.d(t,{NotFound:function(){return a},default:function(){return r}});var e={NotFoundWrap:"NotFound_NotFoundWrap__h0aFF",NotFount:"NotFound_NotFount__DNM2a"},u=n(184),a=function(){return(0,u.jsx)("div",{className:e.NotFoundWrap,children:(0,u.jsx)("h2",{className:e.notFount,children:" Ooops 404 Not Found Page"})})},r=a}}]);
+//# sourceMappingURL=666.50e425e6.chunk.js.map
